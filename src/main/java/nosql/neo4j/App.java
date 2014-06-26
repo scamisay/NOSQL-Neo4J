41446d66fdd5e5f7349918b770dec4ef96b078f8
Teresa <teresa@collossus.com>
2014-06-25 21:10:12 -0300

    starting project again

--- src/main/java/nosql/neo4j/App.java
+++ src/main/java/nosql/neo4j/App.java
@@ -1,7 +1,20 @@
 package nosql.neo4j;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.neo4j.graphdb.DynamicLabel;
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.graphdb.ResourceIterator;
+import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
+import org.neo4j.graphdb.schema.IndexDefinition;
+import org.neo4j.graphdb.schema.Schema;
 
 /**
  * Hello world!
@@ -11,11 +24,32 @@ public class App
 {
 	private static final String DB_PATH = "target/neo4j-hello-db";
 	
+	
     public static void main( String[] args )
     {
-    	GraphDatabaseService graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+    	GraphDatabaseService graphDb;
+    	graphDb = new GraphDatabaseFactory().newEmbeddedDatabase( DB_PATH );
+    	Node firstNode;
+    	Node secondNode;
+    	Relationship relationship;
     	registerShutdownHook( graphDb );
     	System.out.println( "Hello World!" );
+    	
+    	try ( Transaction tx = graphDb.beginTx() )
+    	{
+    	    Label label = DynamicLabel.label( "User" );
+    	    for ( IndexDefinition indexDefinition : graphDb.schema()
+    	            .getIndexes( label ) )
+    	    {
+    	        // There is only one index
+    	        indexDefinition.drop();
+    	    }
+
+    	    tx.success();
+    	}
+    	
+    	firstNode= graphDb.createNode();
+        firstNode.setProperty("date", new Date("1993-04-23") );
     	
     	graphDb.shutdown();
     }
@@ -34,4 +68,12 @@ public class App
             }
         } );
     }
+    
+    private static enum RelTypes implements RelationshipType
+    {
+        KNOWS
+    }
+    
+    
+    
 }
