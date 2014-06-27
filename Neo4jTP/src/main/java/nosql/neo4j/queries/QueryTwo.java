b017bc8f990d9e4fb94d4c107a78bbc4d9f65879
Teresa <teresa@collossus.com>
2014-06-27 18:33:07 -0300

    query 1

---
+++ Neo4jTP/src/main/java/nosql/neo4j/queries/QueryTwo.java
@@ -0,0 +1,20 @@
+package nosql.neo4j.queries;
+
+import java.util.List;
+
+import org.neo4j.graphdb.GraphDatabaseService;
+
+public class QueryTwo extends QueryDB{
+
+	public QueryTwo(GraphDatabaseService db) {
+		super(db);
+	
+	}
+
+	@Override
+	public void execute(List<String> arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
