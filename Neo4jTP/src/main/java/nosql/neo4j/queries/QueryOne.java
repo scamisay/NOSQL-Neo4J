b37d52a2f9c0693c8c2717899542d2165b5b2a8e
Teresa <teresa@collossus.com>
2014-06-27 23:38:32 -0300

    query 1 already written (not tested yet) improving query three

--- Neo4jTP/src/main/java/nosql/neo4j/queries/QueryOne.java
+++ Neo4jTP/src/main/java/nosql/neo4j/queries/QueryOne.java
@@ -1,5 +1,7 @@
 package nosql.neo4j.queries;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,11 +22,17 @@ public class QueryOne extends QueryDB{
 	@Override
 	public void execute(List<String> arguments) {
 		 ExecutionEngine engine = new ExecutionEngine( db );
-
+		 String date1string=arguments.get(0);
+	        int date1year=Integer.parseInt(date1string.substring(0, 3));
+	        int date1month=Integer.parseInt(date1string.substring(5, 6));
+	        int date1day=Integer.parseInt(date1string.substring(8, 9));
+			Calendar calendar = new GregorianCalendar();
+	        calendar.set(date1year, date1month, date1day);
+	        long date1=calendar.getTime().getTime();
 	        ExecutionResult result;
 	        try ( Transaction ignored = db.beginTx() )
 	        {
-	            result = engine.execute( "start l=node:lineItem where l.shipDate <="+""+" return l.returnflag,l.linestatus,sum(l.quantity) AS sum_qty,sum(l.extendedprice) AS sum_base_price,sum(l.extendedprice*(1-l.discount))AS sum_disc_price,sum(l.extendedprice*(1+l.tax)) AS sum_charge,avg(l.quantity) AS avg_qty,avg(l.extendedprice) AS avg_price,avg(l.discount) AS avg_disc,count() AS count_order" );
+	            result = engine.execute( "start l=node:lineItem where l.shipDate <="+date1+" return l.returnflag,l.linestatus,sum(l.quantity) AS sum_qty,sum(l.extendedprice) AS sum_base_price,sum(l.extendedprice*(1-l.discount))AS sum_disc_price,sum(l.extendedprice*(1+l.tax)) AS sum_charge,avg(l.quantity) AS avg_qty,avg(l.extendedprice) AS avg_price,avg(l.discount) AS avg_disc,count() AS count_order order by l.returnflag,l.linestatus" );
 	            // END SNIPPET: execute
 	            // START SNIPPET: items
 	            Iterator<Node> n_column = result.columnAs( "n" );
